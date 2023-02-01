48c189ed50de0bf1bd86bab4781fd5fcf73fda7a
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-02-01 13:57:27 +0530

    updated

--- Server.js
+++ Server.js
@@ -10,155 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// const readline = require('readline');
-
-// const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-// const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-
-// (async() => {
-//   try {
-//     const name = await prompt("Enter name of the stack: ");
-
-//     const total = await prompt("Enter total stack : ");
-    
-//     console.log(`Name: ${name}, total: ${total}`);
-//     let db = new sqlite3.Database("db/stackmanagement.db");
-//     let insertQuery = "INSERT INTO StackTable(name, total) VALUES(?, ?)";
-
-//     const values = [`${name}`,` ${total}`];
-
-//     db.run(insertQuery, values, (err) => {
-//         if(err) {
-//             response.json({
-//                 message: err.message,
-//             });
-//         }
-//     });
-//     db.close();
-//     // rl.close();
-//   } catch (e) {
-//     console.error("Unable to prompt", e);
-//   }
-// })();
-
-
-// When done reading prompt, exit program 
-// rl.on('close', () => process.exit(0));
-
-// (async() => {
-//   try {
-//     const stackId = await prompt("StackId: ");
-//     const name = await prompt("Enter name of the stack: ");
-//     const activity = await prompt("Activity: ");
-//     const value = await prompt("Enter total stack : ");
-    
-//     console.log(`stackId: ${stackId}, Name: ${name}, Activity: ${activity}, value: ${value}`);
-//     let db = new sqlite3.Database("db/stackmanagement.db");
-//     let insertQuery = "INSERT INTO ChangeStack(stackId, name, activity, value) VALUES(?, ?, ?, ?)";
-
-//     const values = [`${stackId}`, `${name}`, `${activity}`, ` ${total}`];
-
-//     db.run(insertQuery, values, (err) => {
-//         if(err) {
-//             response.json({
-//                 message: err.message,
-//             });
-//         // } else {
-//         //     response.json({
-//         //         message: "Succesfully inserted Stack"
-//         //     });
-//         }
-//     });
-//     db.close();
-//     // rl.close();
-//   } catch (e) {
-//     console.error("Unable to prompt", e);
-//   }
-// })();
-
-// rl.on('close', () => process.exit(0));
-
-
-// let db = new sqlite3.Database("db/stackmanagement.db");
-//       let data = [
-//         ['glass', 200],
-//         ['gear', 1000]
-//      ]
-
-//      for (var i=0;i<data.length; i++){
-//         db.run("INSERT INTO StackTable(name, total) values(?,?)",data[i][0],data[i][1],(err,rows)=>{
-//         if(err){
-//            throw err;
-//         }
-//          console.log('Insert Success');
-//     })
-//    }
-//    db.close();
-//    {
-//    let db = new sqlite3.Database("db/stackmanagement.db");
-
-//    let sql = `SELECT name, total FROM StackTable`;
-
-// db.all(sql, [], (err, rows) => {
-//   if (err) {
-//     throw err;
-//   }
-//   rows.forEach((row) => {
-//     console.log(row.name, row.total);
-//   });
-// });
-
-// // close the database connection
-// db.close();}
-// {
-// let db = new sqlite3.Database("db/stackmanagement.db");
-
-// let updatedData = ['200', 11];
-// let updatedQuery = `UPDATE StackTable SET total = ? WHERE stackId = ?`;
-
-// db.run(updatedQuery, updatedData, function(err) {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log(`Row(s) updated: ${this.changes}`);
-
-// });
-
-// // close the database connection
-// db.close();
-
-// }
-
-// {
-//     let db = new sqlite3.Database("db/stackmanagement.db");
-    
-//     let deleteId = [9];
-//     let deleteQuery = `DELETE FROM StackTable WHERE stackId = ?`;
-    
-//     db.run(deleteQuery, deleteId, function(err) {
-//       if (err) {
-//         return console.error(err.message);
-//       }
-//       console.log(`Row(s) Deleted: ${this.changes}`);
-    
-//     });
-    
-//     // close the database connection
-//     db.close();
-    
-//     }
-
-
 app.post("/", (request, response) => {
-    const newStack = request.body;
-    console.log(newStack);
+    const newStock = request.body;
+    console.log(newStock);
 
     let db = new sqlite3.Database("db/stackmanagement.db");
-    let insertQuery = "INSERT INTO StackTable(name, total) VALUES(?, ?)";
+    let insertQuery = "INSERT INTO StockTable(name) VALUES( ?)";
 
     const values = [
-        newStack.name,
-        newStack.total
+        newStock.name,
+        // newStock.total
     ];
 
     db.run(insertQuery, values, (err) => {
@@ -276,13 +137,15 @@ app.post("/change", (request, response) => {
     console.log(changeStack);
 
     let db = new sqlite3.Database("db/stackmanagement.db");
-    // let insertQuery = "INSERT INTO StackChange(name, activity, value, stackId) VALUES(?, ?, ?, ?)";
-    let insertQuery = "INSERT INTO ChangeStack(name, activity, value, stackId) VALUES(?, ?, ?, ?)";
+
+    let insertQuery = "INSERT INTO ChangeStack(name, activity, value, stackId) VALUES(?, ?, ?, ?)"
+
+    const updatedValue = changeStack.activity === 'checkout' ? (changeStack.value * -1) : (changeStack.value);
 
     const values = [
         changeStack.name,
         changeStack.activity,
-        changeStack.value,
+        updatedValue,
         changeStack.stackId
     ];
 
@@ -292,18 +155,50 @@ app.post("/change", (request, response) => {
                 message: err.message,
             });
         } else {
+    const stackId = changeStack.stackId;
+    // const updatedQuery = "UPDATE StackTable SET total = (SELECT  SUM (value) FROM ChangeStack cs WHERE stackId = ?) WHERE stackId = ?";
+    const selectQuery = "SELECT SUM(value) FROM ChangeStack WHERE stackId = ?"
+    db.all(selectQuery, [], (err, rows) => {
+        if(err) {
             response.json({
-                message: "Succesfully inserted Stack"
+                message: err.message,
+            });
+        } else {
+            // const stackEntries = rows.map((singleStack) => {
+            //     return {
+            //         changeId: singleStack.changeId,
+            //         name: singleStack.name,
+            //         value: singleStack.value,
+            //         date:  singleStack.date,
+            //         time: singleStack.time,
+            //         stackId: singleStack.stackId
+            //     };
+            // }); 
+            console.log(rows);
+            const updatedQuery = "UPDATE StackTable SET total = SUM(ChangeStack.value) FROM ChangeStack WHERE StackTable.stackId = ChangeStack.stackId"
+         const values = [stackId];
+
+    db.run(updatedQuery, values, (err) => {
+        if(err) {
+            response.json({
+                message: err.message,
+            });
+        } else {
+            response.json({
+                message: "Stack updated"
             });
         }
     });
+    // console.log(stackEntries);
+        }
+    });} });
     db.close();
 });
 
 app.get("/change", (request, response) => {
     let db = new sqlite3.Database("db/stackmanagement.db");
 
-    const selectQuery = "SELECT changeId, name, value, date, time, stackId FROM StackChange_1";
+    const selectQuery = "SELECT changeId, name, value, date, time, stackId FROM ChangeStack";
 
     db.all(selectQuery, [], (err, rows) => {
         if(err) {
@@ -327,12 +222,6 @@ app.get("/change", (request, response) => {
     db.close();
 });
 
-
-// app.put("/change", (req, res) => {
-//   let db = new sqlite3.Database("db/stackmanagement.db");
-  
-//   const updatedQuery = "UPDATE StackTable st SET st.total = st.total + cs.value FROM StackTable st INNER JOIN ChangeStack cs on st.stackId = cs.stackId where cs.activity = 'checkin' ";
-// })
 
 app.get("/select", (request, response) => {
     let db = new sqlite3.Database("db/stackmanagement.db");
